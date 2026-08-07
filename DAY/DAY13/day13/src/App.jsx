5b4a8cc8aebb0ae0a387df8d59503c28f89bd37a
kavyashreeyp-svg <kavyayp3568@gmail.com>
2026-08-07 12:15:24 +0530

    State ex2: State Toggle

--- DAY/DAY13/day13/src/App.jsx
+++ DAY/DAY13/day13/src/App.jsx
@@ -7,6 +7,7 @@ import InfoBoxDemo from "./components/PropsTwo";
 import './App.css'
 
 import Counter from "./components/State";
+import ToggleText from "./components/StateTwo";
 const name = "Amay";
 const isLoggedIn = false;
 
@@ -38,6 +39,7 @@ function App() {
       <ProfileList />
       <InfoBoxDemo />
       <Counter />
+      <ToggleText />
     </>
     
   )
